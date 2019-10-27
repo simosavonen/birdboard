8b8d68535b6b180202c8707488a514da5f640a92
Simo Savonen <sipesa@utu.fi>
2019-10-27 17:07:07 +0200

    E2: Lets Begin With a Test

---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <h1>Birdboard</h1>
+
+    <ul>
+
+        @foreach ($projects as $project)
+
+            <li>{{ $project->title }}</li>
+
+        @endforeach
+
+    </ul>
+</body>
+</html>
